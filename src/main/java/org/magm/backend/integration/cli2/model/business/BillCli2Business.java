3552b8e6ecbecc5b5e2d2e6a486fb9257feb3045
Faq <69550808+Faq-hue@users.noreply.github.com>
2022-09-08 15:58:35 -0300

    Add files via upload

--- src/main/java/org/magm/backend/integration/cli2/model/business/BillCli2Business.java
+++ src/main/java/org/magm/backend/integration/cli2/model/business/BillCli2Business.java
@@ -1,5 +1,38 @@
 package org.magm.backend.integration.cli2.model.business;
 
-public class BillCli2Business {
+import java.util.List;
+import org.magm.backend.integration.cli2.model.BillCli2;
+
+public class BillCli2Business implements IBillCli2Business{
+
+	@Override
+	public BillCli2 generateBill() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BillCli2 modifyBill(BillCli2 bill) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BillCli2 deleteBill(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BillCli2 getBill(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BillCli2> getBillListNotAnnulled() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
