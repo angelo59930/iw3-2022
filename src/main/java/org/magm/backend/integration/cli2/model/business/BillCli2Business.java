124937f8421d0f970c50c0be0c6bf1ed61f62273
Angelo <manfrediangelosantiago10@gmail.com>
2022-09-08 16:11:12 -0300

    add delete function and exeptios

--- src/main/java/org/magm/backend/integration/cli2/model/business/BillCli2Business.java
+++ src/main/java/org/magm/backend/integration/cli2/model/business/BillCli2Business.java
@@ -2,8 +2,18 @@ package org.magm.backend.integration.cli2.model.business;
 
 import java.util.List;
 import org.magm.backend.integration.cli2.model.BillCli2;
+import org.magm.backend.integration.cli2.model.persistence.IBillCli2Repository;
+import org.magm.backend.model.business.BusinessException;
+import org.magm.backend.model.business.NotFoundException;
+import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
 public class BillCli2Business implements IBillCli2Business{
+
+	private IBillCli2Repository billDAO;
 
 	@Override
 	public BillCli2 generateBill() {
@@ -18,9 +28,15 @@ public class BillCli2Business implements IBillCli2Business{
 	}
 
 	@Override
-	public BillCli2 deleteBill(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public BillCli2 deleteBill(long id) throws NotFoundException, BusinessException{
+		BillCli2 bill = getBill(id);
+		try{
+			billDAO.deleteById(id);
+		} catch(Exception e){
+			log.error(e.getMessage(),e);
+			throw BusinessException.builder().ex(e).build();
+		}
+		return bill;
 	}
 
 	@Override
--- src/main/java/org/magm/backend/integration/cli2/model/business/IBillCli2Business.java
+++ src/main/java/org/magm/backend/integration/cli2/model/business/IBillCli2Business.java
@@ -3,13 +3,15 @@ package org.magm.backend.integration.cli2.model.business;
 import java.util.List;
 
 import org.magm.backend.integration.cli2.model.BillCli2;
+import org.magm.backend.model.business.BusinessException;
+import org.magm.backend.model.business.NotFoundException;
 
 public interface IBillCli2Business {
   public BillCli2 generateBill();
 
   public BillCli2 modifyBill(BillCli2 bill);
 
-  public BillCli2 deleteBill(long id);
+  public BillCli2 deleteBill(long id) throws NotFoundException, BusinessException;
 
   public BillCli2 getBill(long id);
 
