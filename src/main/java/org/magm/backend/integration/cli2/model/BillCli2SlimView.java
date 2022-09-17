5a17d9b1bc4513a11e0ba9bcdffcf853f14b5dd5
Angelo <manfrediangelosantiago10@gmail.com>
2022-09-17 12:51:49 -0300

    finish proyections

--- src/main/java/org/magm/backend/integration/cli2/model/BillCli2SlimView.java
+++ src/main/java/org/magm/backend/integration/cli2/model/BillCli2SlimView.java
@@ -1,5 +1,33 @@
 package org.magm.backend.integration.cli2.model;
 
-public class BillCli2SlimView {
+import java.util.Date;
+
+import org.magm.backend.model.Item;
+import org.magm.backend.model.Product;
+
+public interface BillCli2SlimView {
+  long getNumber();
+
+  Date getIssueDate();
+
+  Date getExpitDate();
+
+  boolean getAnnulled();
+
+  Item getItems();
+
+  //Puede ser que en vez de Items sea Item
+  interface Items {
+    double getQuantity();
+
+    double getPrice();
+
+    // TODO: fijarse si agregar los productos
+    Product getProduct();
+
+    interface Product {
+      String getProduct();
+    }
+  }
   
 }
