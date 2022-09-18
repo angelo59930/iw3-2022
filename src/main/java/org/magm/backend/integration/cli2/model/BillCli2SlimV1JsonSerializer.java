3676dc4cfff069c577e497cbc3c27c5da8f386b9
Angelo <manfrediangelosantiago10@gmail.com>
2022-09-18 11:48:22 -0300

    seriarizer

--- src/main/java/org/magm/backend/integration/cli2/model/BillCli2SlimV1JsonSerializer.java
+++ src/main/java/org/magm/backend/integration/cli2/model/BillCli2SlimV1JsonSerializer.java
@@ -1,5 +1,40 @@
 package org.magm.backend.integration.cli2.model;
 
-public class BillCli2SlimV1JsonSerializer {
-  
+import java.io.IOException;
+
+import org.magm.backend.model.Item;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+public class BillCli2SlimV1JsonSerializer extends StdSerializer<BillCli2> {
+
+  private static final long serialVersionUID = -2957189280265216562L;
+
+  public BillCli2SlimV1JsonSerializer(Class<?> t, boolean dummy) {
+    super(t, dummy);
+  }
+
+  @Override
+  public void serialize(BillCli2 value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+
+    // issueDate, expirationDate, billNumber, annulled, total(cantidad * precio)
+    gen.writeStartObject();
+    gen.writeStringField("version", "v1");
+    gen.writeNumberField("number", value.getNumber());
+    gen.writeObjectField("issueDate", value.getIssueDate());
+    gen.writeObjectField("expirationDate", value.getExpirationDate());
+    gen.writeBooleanField("annulled", value.isAnnulled());
+
+    double total = 0;
+
+    for (Item item : value.getItems()) {
+      total = total + item.getPrice() * item.getQuantity();
+    }
+
+    gen.writeNumberField("total", total);
+    gen.writeEndObject();
+
+  }
 }
--- src/main/java/org/magm/backend/integration/cli2/model/BillCli2SlimV2JsonSerializer.java
+++ src/main/java/org/magm/backend/integration/cli2/model/BillCli2SlimV2JsonSerializer.java
@@ -4,30 +4,39 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import org.magm.backend.integration.cli1.model.ProductCli1JsonDeserializer;
+import org.magm.backend.model.Item;
 import org.magm.backend.util.JsonUtiles;
 
 import java.io.IOException;
 
-public class BillCli2SlimV2JsonSerializer extends StdSerializer<ItemCli2> {
+public class BillCli2SlimV2JsonSerializer extends StdSerializer<BillCli2> {
 
     private static final long serialVersionUID = -4721801379495083847L;
-    protected BillCli2SlimV2JsonSerializer(Class<?> t, boolean dummy) {
+
+    public BillCli2SlimV2JsonSerializer(Class<?> t, boolean dummy) {
         super(t, dummy);
     }
 
     @Override
-    public void serialize(ItemCli2 itemCli2, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+    public void serialize(BillCli2 billCli2, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+            throws IOException {
         jsonGenerator.writeStartObject();
-        jsonGenerator.writeNumberField("id", itemCli2.getId());
-        jsonGenerator.writeStringField("version", "v2");
-        jsonGenerator.writeNumberField("quantity", itemCli2.getQuantity());
-        jsonGenerator.writeNumberField("price", itemCli2.getPrice());
 
-        String productStr = JsonUtiles
-                .getObjectMapper(ProductCli2.class, new ProductCli2SlimV1JsonSerializer(ProductCli2.class, false), null)
-                        .writeValueAsString(itemCli2.getProduct());
-        jsonGenerator.writeFieldName("products");
-        jsonGenerator.writeRawValue(productStr);
+        for (Item itemCli2 : billCli2.getItems()) {
+
+            jsonGenerator.writeNumberField("id", itemCli2.getId());
+            jsonGenerator.writeStringField("version", "v2");
+            jsonGenerator.writeNumberField("quantity", itemCli2.getQuantity());
+            jsonGenerator.writeNumberField("price", itemCli2.getPrice());
+
+            String productStr = JsonUtiles
+                    .getObjectMapper(ProductCli2.class, new ProductCli2SlimV1JsonSerializer(ProductCli2.class, false),
+                            null)
+                    .writeValueAsString(itemCli2.getProduct());
+            jsonGenerator.writeFieldName("products");
+            jsonGenerator.writeRawValue(productStr);
+            
+        }
         jsonGenerator.writeEndObject();
     }
 }
